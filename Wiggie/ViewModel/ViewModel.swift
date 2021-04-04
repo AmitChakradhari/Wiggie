892801fa97d19aa0d8cc1905dd6cebb199b1b84c
amitchakradhari <amitchakradhari2406@gmail.com>
2021-04-05 02:46:12 +0530

    mvc version done

---
+++ Wiggie/ViewModel/ViewModel.swift
@@ -0,0 +1,10 @@
+//
+//  ViewModel.swift
+//  Wiggie
+//
+//  Created by Amit  Chakradhari on 05/04/21.
+//  Copyright © 2021 Amit  Chakradhari. All rights reserved.
+//
+
+import Foundation
+import RxSwift
